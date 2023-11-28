a1b5151c9f58bbde7d1225efeb7025bc0637b5f0
tomerbabila <tomer.babila@gmail.com>
2023-11-28 18:18:09 +0200

    Add elements to home page

--- src/renderer/pages/home/Home.tsx
+++ src/renderer/pages/home/Home.tsx
@@ -1,5 +1,14 @@
 import './Home.scss';
+import { Button } from 'rsuite';
 
 export function Home() {
-  return <div>this is home page</div>;
+  return (
+    <div className="page-container">
+      <h1>Welcome to MusiGit</h1>
+      <div className="actions-container">
+        <Button appearance="primary">Sign In</Button>
+        <Button appearance="ghost">Sign Up</Button>
+      </div>
+    </div>
+  );
 }
